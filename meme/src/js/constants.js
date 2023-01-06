dc58d5c151f97b22157c68615eb60403f3f9e9d3
Júnior Gonçalves <lyestah@yahoo.com.br>
2023-01-06 14:20:57 -0300

    Deletado dir

--- meme/src/js/constants.js
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_GENERATED_FILE_NAME = 'meme.png';
-export const ACCEPTED_MIME_TYPES = ['image/jpg', 'image/jpeg', 'image/png', 'image/apng', 'image/gif', 'image/webp', 'image/avif'];
--- meme/workbox-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globDirectory: 'docs',
-  globPatterns: [
-    '**/*.{html,js,css,png,svg,jpg,gif,json,ttf,woff,woff2,eot,ico,webmanifest,map}'
-  ],
-  swDest: 'docs/service-worker.js',
-  clientsClaim: true,
-  skipWaiting: true
-};
